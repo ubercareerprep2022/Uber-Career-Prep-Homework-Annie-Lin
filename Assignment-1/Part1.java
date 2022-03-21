ec1430da1bfaf426eb6dc61dbbd0beb27af0db4c
yyannielin <77912633+yyannielin@users.noreply.github.com>
2022-03-20 23:29:00 -0400

    attempted part1

--- Assignment-1/Part1.java
+++ Assignment-1/Part1.java
@@ -1,9 +1,29 @@
 // isStringPermutation(...)
-// Implement the function isStringPermutation() that takes two Strings as parameters and returns a Boolean denoting whether the first string is a permutation of the second string.
-// Go-Lang	func isStringPermutation(s1 string, s2 string) bool {}
-// Swift		func isStringPermutation(s1: String, s2: String) -> Bool {}
-// Python 3	def isStringPermutation(s1: str, s2: str) -> bool:
-// Java		public boolean isStringPermutation(String s1, String s2) {}
+// Implement the function isStringPermutation() that takes two Strings as parameters 
+// and returns a Boolean denoting whether the first string is a permutation of the second string.
+
+// Java		
+
+
+public boolean isStringPermutation(String s1, String s2) {
+
+    if (s1.length() != s2.length()) return false;
+
+    char arr1[] = s1.toCharArray();
+    char arr2[] = s2.toCharArray();
+
+    Arrays.sort(arr1);
+    Arrays.sort(arr2);
+
+    for (int i = 0; i < arr1.length; i++){
+        if (arr1[i] != arr2[i]) return false;
+    }
+
+    return true;
+
+}
+
+
 // Below are some examples:
 // isStringPermutation(s1: “asdf”, s2: “fsda”) == true
 // isStringPermutation(s1: “asdf”, s2: “fsa”) == false
