f1f40fc66efa685fdaa37dc6c4c58f4ec1f647ee
Annie Lin <yl725@duke.edu>
2022-08-09 23:30:36 -0400

    ex2-3

--- Assignment-3/Ex2.java
+++ Assignment-3/Ex2.java
@@ -5,6 +5,14 @@
 // load 100 entries at a time, sort 
 // merge in the end 
 
+// input: 
+// A = [[0,50,...,3,8,9],[21,90,...,32],...[1900,48021,...24891]]
+// 1,000,000 / 100 = 10,000
+// loop thru 10000 entries of A and implement merge sort on each entry
+// A = [[0,3,8,9,50...],[21,32,90,...],...,[1900,24891,48021,...]]
+// 10,000 / 100 = 10
+// ???
+
 Class ExternalSort{
     static void main(String[] args){
 
@@ -17,21 +25,41 @@ Class ExternalSort{
 		System.out.println();
 	}
 
-    static int[] merge(int arr[], int lo, int hi){
+    static int[] merge(int arr[], int lo, int mid, int hi){
 
+        int[] temp = new int[hi - lo + 1];
+        int k = 0;
+        int p = lo; // iterate thru first half
+        int q = mid + 1; // iterate thru second half 
+
+        for (int i = 0; i < temp.length; i++){
+            if (p > mid) {
+                temp[k++] = arr[q];
+            } else if (q > hi){
+                temp[k++] = arr[p];
+            } else if (arr[p] < arr[q]){
+                temp[k++] = arr[p++];
+            } else {
+                temp[k++] = arr[q++];
+            } 
+        }
+
+        for (int i = 0; i < temp.length; i++){
+            arr[i] = temp[i];
+        }
+
+        return arr;
     }
 
-    static int[] mergeSort(int arr[]){
-        while (lo < hi){
-            int mid = arr.length / 2;
-            int lo = 0;
-            int hi = arr.length - 1;
+    static int[] mergeSort(int arr[], int lo, int hi){
 
+        while (lo < hi){ // if we don't have the while loop, recursion will be infinite 
+            int mid = arr.length / 2;
             mergeSort(arr[], lo, mid-1);
             mergeSort(arr[], mid, hi);
         }
 
-        merge(arr[]);
+        merge(arr[], lo, mid, hi);
     }
 
 }
---
+++ Assignment-3/Ex3.java
@@ -0,0 +1,24 @@
+/* Sorting Exercise 3: Sorted Merge 
+You are given two sorted arrays, A and B, where A has a large enough buffer at the
+end to hold B. Write a method to merge B into A in sorted order in one pass and using O(1) space. 
+ */
+
+/* 
+ * Test case 1:
+ * A: [1,4,7,9]
+ * B: [2,6,9,10]
+ * 
+ * Test case 2:
+ * A: [1,4,7,9]
+ * B: [2,6,8,9,10,15,16]
+ * 
+ * Method:
+ * 
+ * choose whichever array is longer -> we will add elements to this longArray
+ * compare, add, and shift
+ * 
+ */
+
+Class SortedMerge{
+    static 
+}
