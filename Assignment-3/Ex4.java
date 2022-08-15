d7f830dcf6ce0cff783949b1baf905fb979137ae
yyannielin <77912633+yyannielin@users.noreply.github.com>
2022-08-15 19:11:39 -0400

    completed ex4

--- Assignment-3/Ex4.java
+++ Assignment-3/Ex4.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /*
 Sorting Exercise 4: Group Anagrams
 Write a method to sort an array of strings so that all the anagrams are next to each other. 
@@ -13,6 +14,7 @@ Input: ["cat", "dog", "tac", "god", "act"]
  * Approach 1:
  * build a helper to break down each string
  * "cat" = ["c","a","t"]
+ * sort the array 
  * 
  * create a hashmap that groups words with same breakdowns 
  * Map<String, ArrayList> = new HashMap<>()
@@ -22,6 +24,72 @@ Input: ["cat", "dog", "tac", "god", "act"]
  * 
  */
 
+ //https://www.geeksforgeeks.org/given-a-sequence-of-words-print-all-anagrams-together/
+
 public class Ex4 {
+
+    public static void main(String[] args){
+        String[] input = new String[]{"cat", "dog", "tac", "god", "act"};
+        groupAnas(input);
+    }
+
+    // Input: ["cat", "dog", "tac", "god", "act"]
+    // map: 
+    // ["a","c","t"]: ["cat","tac"]
+    // ["d","o","g"]: ["dog"]
+
+    public static ArrayList<String> groupAnas(String[] anas){
+
+        Map<char[], ArrayList<String>> map = new HashMap<char[], ArrayList<String>>();
+
+        for (String s : anas){
+            char[] sorted = sortChars(s);
+            if (!map.containsKey(sorted)){
+                ArrayList<String> list = new ArrayList<String>();
+                list.add(s);
+                map.put(sorted, list);
+            } else {
+                ArrayList<String> list = map.get(sorted);
+                list.add(s);
+                map.put(sorted, list);
+            }
+        }
+
+        ArrayList<String> ret = mapToList(map);
+        // printList(ret);
+        return ret;
+    }
+
+    // input: "cat"
+    public static char[] sortChars(String word){
+        char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+        printArray(chars);
+        return chars;
+    }
+
+    public static ArrayList<String> mapToList(Map<char[], ArrayList<String>> map){
+        ArrayList<String> vals = new ArrayList<String>();
+        for (ArrayList<String> v : map.values()){
+            vals.addAll(v);
+        }
+        return vals;
+    }
+
+    static void printList(ArrayList<String> list)
+	{
+		int n = list.size();
+		for (int i=0; i<n; ++i)
+			System.out.print(list.get(i)+" ");
+		System.out.println();
+	}
+
+    static void printArray(char arr[])
+	{
+		int n = arr.length;
+		for (int i=0; i<n; ++i)
+			System.out.print(arr[i]+" ");
+		System.out.println();
+	}
     
 }
